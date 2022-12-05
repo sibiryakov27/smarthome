7078d89e43126ec982ad7e3ad08b4fb1bdb81d23
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2022-12-05 14:20:53 +0300

    Add new module and copy some files from old projects

---
+++ smarthome-config/src/main/java/ru/config/smarthome/SmarthomeConfigApplication.java
@@ -0,0 +1,13 @@
+package ru.config.smarthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthomeConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarthomeConfigApplication.class, args);
+    }
+
+}
---
+++ smarthome-config/src/test/java/ru/config/smarthome/SmarthomeConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.config.smarthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
